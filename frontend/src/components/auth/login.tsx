8eff8510b31db9ed3d77041a1603b04b54da65ef
Brian Perrett <perrettbrian@gmail.com>
2021-11-19 20:41:02 -0700

    use react-hook-form on login page

--- frontend/src/components/auth/login.tsx
+++ frontend/src/components/auth/login.tsx
@@ -1,19 +1,25 @@
-import { useState } from "react";
 import { useMutation } from "react-query";
+import { useForm, SubmitHandler } from 'react-hook-form';
 import * as api from "../../api/api";
 import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
 import Alert from 'react-bootstrap/Alert';
 
+interface LoginFormType {
+  email: string,
+  password: string,
+}
 export function Login() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const form = useForm<LoginFormType>();
 
-  const loginMut = useMutation(() => {
-    return api.account.login(email, password).then((response) => {
-      window.location.href = '/'
-    });
-  });
+  const loginMut = useMutation(
+    (data: LoginFormType) => api.account.login(data.email, data.password),
+    {
+      onSuccess: () => {window.location.href = '/'},
+    }
+  );
+  const onSubmit: SubmitHandler<LoginFormType> = (data) => {
+    loginMut.mutate(data)
+  }
 
   return (
     <>
@@ -23,23 +29,21 @@ export function Login() {
             There was an error while trying to log in.
           </Alert>
         }
-        <Form>
+        <Form onSubmit={form.handleSubmit(onSubmit)}>
           <Form.Label>Email Address</Form.Label>
           <Form.Control
+            {...form.register('email', {required: true})}
             type="email"
             placeholder="Enter Email"
-            value={email}
-            onChange={(event) => setEmail(event.target.value)}
           />
 
           <Form.Label>Password</Form.Label>
           <Form.Control
+            {...form.register('password', {required: true})}
             type="password"
             placeholder="Enter Password"
-            value={password}
-            onChange={(event) => setPassword(event.target.value)}
           />
-          <Button onClick={() => loginMut.mutate()}>Login</Button>
+          <input className="btn btn-primary" type="submit" />
         </Form>
       </div>
     </>
